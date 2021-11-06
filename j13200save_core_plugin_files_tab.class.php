60cc4e9a4bffeeb624b7bf71d307a58574017b6e
Lopez <lopez.a@bluejaypm.com>
2021-11-05 19:52:23 -0700

    Add files via upload

---
+++ j13200save_core_plugin_files_tab.class.php
@@ -0,0 +1,36 @@
+<?php
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( 'Direct Access to this file is not allowed.' );
+// ################################################################
+
+class j13200save_core_plugin_files_tab {
+	function __construct($componentArgs)
+		{
+		$MiniComponents =jomres_getSingleton('mcHandler');
+		if ($MiniComponents->template_touch)
+			{
+			$this->template_touchable=false; return;
+			}
+
+		$this->ret_vals = array  ( "TAB_ID" => "save_core_plugin_files" , "TAB_NAME" => "Save Core Plugin Files" , "TAB_CONTENTS" => "<table><tr>
+				<td>This Plugin will Save your Jomres Core Plugins files for later use if you did not need them now and after your Jomres Subscription has expired.<br><br>
+				 After this plugin is installed; All you need to do is just hit the blue \"Install\" or \"Reinstall\" button under the \"Add/reinstall/upgrade plugin\" column.
+				<br><br>
+				ALL your Jomres Core Plugins files are now saved here as a ZIP file: /jomres/updates/<br><br>
+				You can even save your Plugin Manager files for later!<br><br>
+				Check it out!<br><br>
+				These \"Core Plugins\" files are the original files from Jomres Server before they are installed.<br><br>
+				<b>DON'T FORGET TO DOWNLOAD THE CORE FILES AND SAVE THEM FOR LATER!</b>
+				</td>
+				</tr></table>
+") ;
+		}
+
+	// This must be included in every Event/Mini-component
+	function getRetVals()
+		{
+		return $this->ret_vals;
+		}
+	}
+?>
---
+++ plugin_info.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * @version Jomres 9.23.x
+ *
+ * @copyright	2021 Mark the Shark
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly
+ *
+ **/
+ 
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+class plugin_info_save_core_plugin_files
+	{
+	function __construct()
+		{
+		$this->data=array(
+			"name"=>"save_core_plugin_files",
+			"category"=>"System",
+			"marketing"=>"This Plugin will Save your Jomres Core Plugins files for later use if you did not need it not and after your Jomres Subscription has expires. After this plugin is installed; All you need to do is just hit the blue Install or Reinstall button under the Add/reinstall/upgrade plugin column.",
+			"version"=>"1.00",
+			"description"=> "This Plugin will Save your Jomres Core Plugins files for later use if you did not need them now and after your Jomres Subscription has expired. After this plugin is installed; All you need to do is just hit the blue \"Install\" or \"Reinstall\" button under the \"Add/reinstall/upgrade plugin\" column.",
+			"lastupdate"=>"11/05/2021",
+			"min_jomres_ver"=>"9.23.0",
+			'third_party_plugin_latest_available_version' => "http://www.fixmycomputermark.com/jomrespluginsinfo/save_core_plugin_files.json",
+			"manual_link"=>'',
+			'change_log'=>'None.',
+			'highlight'=>'',
+			'image'=>'https://snippets.jomres.net/plugin_screenshots/2017-08-02_4tti8.png',
+			'demo_url'=>'',
+			 "developer_page" => "http://www.fixmycomputermark.com/free-downloads/file/129-save-jomres-core-plugin-files.html",
+			"author"=>"Mark",
+			"authoremail"=>"<a href=mailto:info@fixmycomputermark.com?subject=save_core_plugin_files>Email_Me</a>"
+			);
+		}
+	}
+?>
